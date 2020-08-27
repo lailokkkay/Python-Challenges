8a57a8207a0be02139db50fd134709b5da88a73e
leonardlailokkay <69658860+leonardlailokkay@users.noreply.github.com>
2020-08-27 12:47:24 +0800

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,11 +1,8 @@
 import random
 moves = ["Rock", "Paper", "Scissors"]
-
 playerMove = int(input("0 for Rock, 1 for Paper, 2 for Scissors : "))
-
-computerMove = random.randint(1,2)
-
-if computerMove == 1:
+winCondition = random.randint(1,2)
+if winCondition == 1:
     print(moves[playerMove] + " beats " + moves[playerMove - 1] + ". You Win!")
-elif computerMove == 2:
+elif winCondition == 2:
     print(moves[playerMove + 1] + " beats " + moves[playerMove] + ". You Lose!") 
