078f72fb8759fff379bc41ea17158d41bbaee448
leonardlailokkay <69658860+leonardlailokkay@users.noreply.github.com>
2020-09-02 13:01:34 +0800

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,9 @@
 for i in range(1, 21):
     out = ""
-    x = False
     if i % 3 == 0:
             out += "Fizz"
-            x = True
     if i % 5 == 0:
             out += "Buzz"
-            x = True
-    if x != True:
+    if out == "":
         out = i
     print(out)
-
-
