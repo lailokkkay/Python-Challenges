7a42b85a072ade0edbff6d4c2795a19300c456ba
lailokkkay <69658860+lailokkkay@users.noreply.github.com>
2021-09-22 14:48:49 +0800

    Update TowerOfHanoi.py

--- TowerOfHanoi.py
+++ TowerOfHanoi.py
@@ -1,11 +1,7 @@
-def solve_toh(n_disks):
-    if n_disks == 0:
-        return
-    solve_toh(n_disks-1)
-    move_disk(n_disks)
-    solve_toh(n_disks-1)
-
-def move_disk(n_disk):
-    print("Move", str(n_disk))  
-
-solve_toh(3)
+def solve_toh(n_disks, source, mid, target):
+    if n_disks == 1:
+        return print("move disk",n_disks, "from", source, "to", target)
+    solve_toh(n_disks-1, source, target, mid)
+    print("move disk",n_disks, "from", source, "to", target)
+    solve_toh(n_disks-1, mid, source, target)
+solve_toh(3, 'A', 'B', 'C')
